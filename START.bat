d7a9284fd2927d8e29f640b7858fc8450eff25aa
Michael55555 <michaelpeters842@gmail.com>
2018-03-07 17:48:40 +0100

    Code cleanup

--- START.bat
+++ START.bat
@@ -1,9 +1,8 @@
 @echo OFF
 
 reg Query "HKLM\Hardware\Description\System\CentralProcessor\0" | find /i "x86" > NUL && set OS=32BIT || set OS=64BIT
-if %OS%==32BIT start autobrowser(x86).exe
-if %OS%==32BIT start WinActive(x86).exe
+if %OS%==32BIT start files\autobrowser(x86).exe
+if %OS%==32BIT start files\WinActive(x86).exe
  
-if %OS%==64BIT	start autobrowser(x64).exe
-if %OS%==64BIT	start WinActive(x64).exe
-PAUSE
+if %OS%==64BIT	start files\autobrowser(x64).exe
+if %OS%==64BIT	start files\WinActive(x64).exe
